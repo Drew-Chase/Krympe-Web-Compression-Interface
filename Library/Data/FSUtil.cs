d7b7586eae7c1a4c7a1f09e0e49e9de342c151bc
Drew Chase <dcmanproductions@gmail.com>
2022-06-09 12:31:18 -0400

    Added File System Utility

---
+++ Library/Data/FSUtil.cs
@@ -0,0 +1,41 @@
+﻿using Krympe.Library.Objects;
+
+namespace Krympe.Library.Data;
+
+public static class FSUtil
+{
+    #region Public Methods
+
+    public static FolderItem[] GetDirectoryEntries(string path)
+    {
+        List<FolderItem> entries = new();
+        foreach (string dir in Directory.GetFileSystemEntries(path))
+        {
+            entries.Add(new(dir));
+        }
+        return entries.ToArray();
+    }
+
+    public static object GetDrives()
+    {
+        object[] objs = new object[DriveInfo.GetDrives().Length];
+        for (int i = 0; i < DriveInfo.GetDrives().Length; i++)
+        {
+            DriveInfo drive = DriveInfo.GetDrives()[i];
+            objs[i] = new
+            {
+                Name = drive.Name,
+                TotalSize = drive.TotalSize,
+                TotalFreeSpace = drive.TotalFreeSpace,
+                AvailableFreeSpace = drive.AvailableFreeSpace,
+            };
+        }
+        object obj = new
+        {
+            Drives = objs
+        };
+        return obj;
+    }
+
+    #endregion Public Methods
+}
