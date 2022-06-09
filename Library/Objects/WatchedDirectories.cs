cf0188de9b850e4ccf5e2c3b8cf691ac162514df
Drew Chase <dcmanproductions@gmail.com>
2022-06-09 12:36:47 -0400

    Added List for Watched Directories

---
+++ Library/Objects/WatchedDirectories.cs
@@ -0,0 +1,34 @@
+﻿namespace Krympe.Library.Objects;
+
+public class WatchedDirectories
+{
+    #region Fields
+
+    public static WatchedDirectories Instance = Instance ??= new();
+    private List<FolderItem> _folders;
+
+    #endregion Fields
+
+    #region Protected Constructors
+
+    protected WatchedDirectories()
+    {
+        _folders = new List<FolderItem>();
+    }
+
+    #endregion Protected Constructors
+
+    #region Public Methods
+
+    public void Add(string path)
+    {
+        _folders.Add(new(path));
+    }
+
+    public FolderItem[] Get()
+    {
+        return _folders.ToArray();
+    }
+
+    #endregion Public Methods
+}
