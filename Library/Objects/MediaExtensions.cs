810d8bb63e5025d9781ebd4647390176da6d1115
Drew Chase <dcmanproductions@gmail.com>
2022-06-09 12:36:38 -0400

    Added Media Extensions

---
+++ Library/Objects/MediaExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+namespace Krympe.Library.Objects
+{
+    public class MediaExtensions
+    {
+        #region Fields
+
+        private List<string> ext;
+
+        #endregion Fields
+
+        #region Protected Constructors
+
+        protected MediaExtensions(List<string> ext)
+        {
+            this.ext = ext;
+        }
+
+        #endregion Protected Constructors
+
+        #region Public Methods
+
+        public static MediaExtensions Make(string extList)
+        {
+            return new(extList.Split(";").ToList());
+        }
+
+        public void Add(string extension)
+        {
+            ext.Add(extension);
+        }
+
+        public string[] Get()
+        {
+            return ext.ToArray();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new();
+            foreach (string s in ext)
+            {
+                builder.Append(s);
+            }
+            for (int i = 0; i < ext.Count; i++)
+            {
+                builder.Append(ext[i]);
+                if (i < ext.Count - 1)
+                {
+                    builder.Append(";");
+                }
+            }
+            return builder.ToString().Trim();
+        }
+
+        #endregion Public Methods
+    }
+}
