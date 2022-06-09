78f2285f15d7cd44b43e363d273d13fc72e3a1b2
Drew Chase <dcmanproductions@gmail.com>
2022-06-09 12:36:25 -0400

    Added File Item

---
+++ Library/Objects/FileItem.cs
@@ -0,0 +1,41 @@
+﻿namespace Krympe.Library.Objects;
+
+public class FileItem
+{
+    #region Fields
+
+    private FileInfo info;
+
+    #endregion Fields
+
+    #region Public Constructors
+
+    public FileItem(string path)
+    {
+        Path = path;
+        Refresh();
+    }
+
+    #endregion Public Constructors
+
+    #region Properties
+
+    public string Name { get; private set; }
+    public string Path { get; init; }
+
+    public long Size { get; private set; }
+
+    #endregion Properties
+
+    #region Public Methods
+
+    public void Refresh()
+    {
+        info = new(Path);
+        Name = info.Name;
+
+        Size = info.Length;
+    }
+
+    #endregion Public Methods
+}
