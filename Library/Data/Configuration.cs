e7e0374fa04438bb2df0c8cc3800712268464420
Drew Chase <dcmanproductions@gmail.com>
2022-06-14 18:42:52 -0400

    Added configuration options

--- Library/Data/Configuration.cs
+++ Library/Data/Configuration.cs
@@ -1,6 +1,7 @@
 ﻿using ChaseLabs.CLConfiguration.List;
 using CLPortmapper;
 using Krympe.Library.Objects;
+using Newtonsoft.Json.Linq;
 
 namespace Krympe.Library.Data;
 
@@ -10,6 +11,7 @@ public class Configuration
 
     public static Configuration Instance = Instance ??= new Configuration();
 
+    private string[] _watched_paths = null;
     private ConfigManager manager;
 
     #endregion Fields
@@ -18,23 +20,30 @@ public class Configuration
 
     protected Configuration()
     {
-        manager = new(Path.Combine(Global.ConfigDirectory, "app.cfg"));
-        manager.Add("Port", 2505);
-        manager.Add("Port Forward", false);
-        manager.Add("Admin Token", Guid.NewGuid().ToString());
-        manager.Add("extensions", MediaExtensions.Make("mpegg;mpeg;mp4;mkv;m4a;m4v;f4v;f4a;m4b;m4r;f4b;mov;3gp;3gp2;3g2;3gpp;3gpp2;ogg;oga;ogv;ogx;wmv;wma;flv;avi").ToString());
+        manager = new(Path.Combine(ConfigDirectory, "app.json"));
+        manager.Add("port", 2505);
+        manager.Add("port_forward", false);
+        manager.Add("overwrite_original", false);
+        manager.Add("concurrent_processes", 3);
+        manager.Add("temp_directory", Directory.CreateDirectory(Path.Combine(RootDirectory, "temp")).FullName);
+        manager.Add("ffmpeg", "ffmpeg -hwaccel auto -y -i {INPUT} -b:v 0 -c:v h264 -b:a 0 -c:a aac {OUTPUT}");
+        manager.Add("extensions", MediaExtensions.Make("mpegg;mpeg;mp4;mkv;m4a;m4v;f4v;f4a;m4b;m4r;f4b;mov;3gp;3gp2;3g2;3gpp;3gpp2;ogg;oga;ogv;ogx;wmv;wma;flv;avi".Split(";")).Get());
+        manager.Add("watched", Array.Empty<string>());
     }
 
     #endregion Protected Constructors
 
     #region Properties
 
-    public MediaExtensions Extensions { get => MediaExtensions.Make(manager.GetConfigByKey("extensions").Value); }
-    public int Port { get => manager.GetConfigByKey("Port").Value; set => manager.GetConfigByKey("Port").Value = value; }
+    public int ConcurrentProcesses { get => manager.GetConfigByKey("concurrent_processes").Value; set => manager.GetConfigByKey("concurrent_processes").Value = value; }
+    public MediaExtensions Extensions { get => MediaExtensions.Make(manager.GetConfigByKey("extensions").Value); set => manager.GetConfigByKey("extensions").Value = value; }
+    public string FFMpeg_Command { get => manager.GetConfigByKey("ffmpeg").Value; set => manager.GetConfigByKey("ffmpeg").Value = value; }
+    public bool OverwriteOriginal { get => manager.GetConfigByKey("overwrite_original").Value; set => manager.GetConfigByKey("overwrite_original").Value = value; }
+    public int Port { get => manager.GetConfigByKey("port").Value; set => manager.GetConfigByKey("port").Value = value; }
 
     public bool PortForward
     {
-        get => manager.GetConfigByKey("Port Forward").Value;
+        get => manager.GetConfigByKey("port_forward").Value;
         set
         {
             try
@@ -51,11 +60,40 @@ public class Configuration
             catch
             {
             }
-            manager.GetConfigByKey("Port Forward").Value = value;
+            manager.GetConfigByKey("port_forward").Value = value;
         }
     }
 
-    public Guid Token { get => Guid.Parse(manager.GetConfigByKey("Admin Token").Value); set => manager.GetConfigByKey("Admin Token").Value = value.ToString(); }
+    public string TempDirectory
+    {
+        get => manager.GetConfigByKey("temp_directory").Value; set
+        {
+            Directory.Delete(TempDirectory, true);
+            manager.GetConfigByKey("temp_directory").Value = Directory.CreateDirectory(value).FullName;
+        }
+    }
+
+    public string[] WatchedPaths
+    {
+        get
+        {
+            if (_watched_paths == null)
+            {
+                List<string> paths = new();
+                foreach (JToken token in manager.GetConfigByKey("watched").Value)
+                {
+                    paths.Add(token.ToString());
+                }
+                _watched_paths = paths.ToArray();
+            }
+            return _watched_paths;
+        }
+        set
+        {
+            _watched_paths = value;
+            manager.GetConfigByKey("watched").Value = value;
+        }
+    }
 
     #endregion Properties
 
