bcf78aa7111a831ed8f190e1798d34e39100f6db
Drew Chase <dcmanproductions@gmail.com>
2022-06-16 19:09:29 -0400

    Added Check FFmpeg Command Function and hide the ffmpeg window

--- Library/Data/FFmpegUtil.cs
+++ Library/Data/FFmpegUtil.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using Krympe.Library.Objects;
+using System.Diagnostics;
+using Xabe.FFmpeg;
 using Xabe.FFmpeg.Downloader;
 
 namespace Krympe.Library.Data;
@@ -44,15 +46,42 @@ public class FFmpegUtil
 
     #region Public Methods
 
-    public Process RunFFmpegCommand(string inputFile, string arguments, string outputFile)
+    public bool CheckFFmpegCommand()
     {
+        log.Info("Checking FFMPEG Command");
+        string command = Configuration.Instance.FFMpeg_Command;
+        Random random = new();
+        FolderItem[] folders = WatchedDirectories.Instance.Get();
+        FileItem[]? files = FSUtil.GetFiles(folders[random.Next(0, folders.Length - 1)]);
+        if (files == null)
+            return CheckFFmpegCommand();
+        string input = files[random.Next(0, files.Length - 1)].Path;
+        string output = Path.Combine(TempDirectory, "test_file.mp4");
+        if (FFmpeg.GetMediaInfo(input).Result.Duration.TotalSeconds < 10)
+        {
+            return CheckFFmpegCommand();
+        }
+        command = command.Replace("{OUTPUT}", "-t 1 {OUTPUT}");
+        Process process = RunFFmpegCommand(input, command, output);
+        process.Start();
+        process.WaitForExit();
+        File.Delete(output);
+        return process.ExitCode == 0;
+    }
+
+    public Process RunFFmpegCommand(string inputFile, string outputFile) => RunFFmpegCommand(inputFile, Configuration.Instance.FFMpeg_Command, outputFile);
+
+    public Process RunFFmpegCommand(string inputFile, string command, string outputFile)
+    {
+        log.Debug($"Running FFMPEG Command {command.Replace("{INPUT}", $"\"{inputFile}\"").Replace("{OUTPUT}", $"\"{outputFile}\"").Trim()}");
         return new()
         {
             StartInfo = new()
             {
                 FileName = ffmpeg_exe,
-                Arguments = arguments.Replace("{INPUT}", $"\"{inputFile}\"").Replace("{OUTPUT}", $"\"{outputFile}\"").Replace("ffmpeg", "").Trim(),
-                UseShellExecute = true,
+                Arguments = command.Replace("{INPUT}", $"\"{inputFile}\"").Replace("{OUTPUT}", $"\"{outputFile}\"").Replace("ffmpeg", "").Trim(),
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden,
             }
         };
     }
