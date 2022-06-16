e5e9deebde49ec0239f0b6bdaf4d9fcfa07b73e4
Drew Chase <dcmanproductions@gmail.com>
2022-06-16 19:04:54 -0400

    Added button to test ffmpeg command

--- WebAPI/wwwroot/assets/js/compression.js
+++ WebAPI/wwwroot/assets/js/compression.js
@@ -9,6 +9,39 @@
             BuildCommand();
         })
     })
+    $("#test-ffmpeg-command-btn").on('click', async e => {
+        let button = e.currentTarget;
+        if (!button.classList.contains('disabled')) {
+            let success = `<i class="fa-solid fa-circle-check"></i>`;
+            let failed = `<i class="fa-solid fa-circle-xmark"></i>`;
+            let unset = `<i class="fa-solid fa-vial"></i>`;
+            let loading = `<i class="loading"></i>`;
+            button.innerHTML = "";
+            button.classList.add('disabled')
+            button.innerHTML = loading;
+
+            let response = await fetch('/api/process/test');
+            let json = await response.json();
+
+            if (json["working"] == true) {
+                button.innerHTML = success
+                button.style.backgroundColor = "green"
+            } else {
+                button.innerHTML = failed
+                button.style.backgroundColor = "red"
+            }
+            button.classList.remove("loading");
+
+            setTimeout(() => {
+                button.innerHTML = "";
+                button.innerHTML = unset
+                button.classList.remove("loading");
+                button.style.backgroundColor = ""
+                button.classList.remove("disabled");
+            }, 5 * 1000);
+        }
+    })
+
     BuildCommand();
     function BuildCommand() {
         let cmd = `ffmpeg`
